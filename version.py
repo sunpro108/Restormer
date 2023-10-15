f447b557759e273e55a626963bc852cbbb5a4005
sunpro108 <sunpro108@163.com>
2023-10-15 20:05:00 +0800

    version version0.0.1

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Mar  9 22:05:30 2022
-__version__ = '1.2.0+10018c6'
-short_version = '1.2.0'
-version_info = (1, 2, 0)
+# TIME: 2023-10-15 20:03:16
+__version__ = '0.0.1+b6163f'
+short_version = '0.0.1'
+version_info = (0, 0, 1)
